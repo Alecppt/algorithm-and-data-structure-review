d04b84148dac94a30f884548b057804dd1323fd4
Alec Chen <goodneighboralec@gmail.com>
2019-03-14 10:07:01 -0700

    practice bottomup mergesort implementation

--- SortTest.java
+++ SortTest.java
@@ -1,10 +1,12 @@
 import com.alecppt.quicksort.QuickSort;
+import com.alecppt.mergesort.MergeSort;
 
 public class SortTest {
 
     public static void main(String[] args) {
         int[] num = new int[] { 93, 29, 3, 22, 25, 499, 29, 1, 23, 5};
-        QuickSort.quick_sort(num);
+        // QuickSort.quick_sort(num);
+        num = MergeSort.mergesort(num);
         for ( int i : num)
         {
             System.out.print(i + " ");
---
+++ mergesort/MergeSort.java
@@ -0,0 +1,59 @@
+package com.alecppt.mergesort;
+
+
+public  class MergeSort 
+{
+    public static   int[] mergesort(int[] array)
+    {
+        if(array.length < 2) return array;
+        int mid =  array.length /2;
+        int[] a = new int[mid];
+        int[] b = new int[array.length - mid];
+        for (int i = 0; i<array.length; i++)
+        {
+            if(i < mid)
+            {
+                a[i] = array[i];
+            }
+            else
+            {
+                b[i-mid] = array[i];
+            }
+            
+        }   
+       return  merge(mergesort(a), mergesort(b));
+    }
+
+    private static int[] merge(int[] a, int[]b)
+    {
+        int[] temp = new int[a.length + b.length];
+        int a_index = 0, b_index = 0;
+        for(int i = 0; i < temp.length; i++)
+        {
+            if(a_index >= a.length) 
+            {
+                temp[i] = b[b_index];
+                b_index++;
+            }
+            else if (b_index >= b.length)
+            {
+                temp[i] = a[a_index];
+                a_index++;
+            }
+            else if (a[a_index]<= b[b_index] && (a_index < a.length && b_index < b.length))
+            {
+                temp[i] = a[a_index];
+                a_index++;
+            }
+            else
+            {
+                temp[i] = b[b_index];
+                b_index++;
+            }
+        }
+
+
+
+        return temp;
+    }
+}
