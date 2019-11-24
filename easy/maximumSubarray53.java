9015ccf2b4d9a6a0ae51a503f4bcd68c12d5add1
Alec Chen <goodneighboralec@gmail.com>
2019-11-24 09:48:35 -0800

    53 solved

---
+++ easy/maximumSubarray53.java
@@ -0,0 +1,21 @@
+package easy;
+
+class maximumSubarray53 
+{
+    public static void main(String[] args) {
+        int a[]  ={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maximumSubarray(a));
+        //should be 6 [4, -1, 2, 1]
+    }
+    
+    static int maximumSubarray(int nums[])
+    {
+        int globalMax = nums[0], currentMax = nums[0];
+        for( int i = 1; i < nums.length; i++)
+        {
+            currentMax = Math.max(nums[i], nums[i] + currentMax);
+            globalMax = Math.max(globalMax, currentMax);            
+        }
+        return globalMax;
+    }
+}
