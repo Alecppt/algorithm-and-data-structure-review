6740280c699f73e9de6e766928890d0769a61b68
Alec Chen <goodneighboralec@gmail.com>
2020-08-07 01:43:26 -0700

    optimize from O(n^3) to O(n^2)

--- medium/03_Longest_Substring_Without_Repeating_Characters.java
+++ medium/03_Longest_Substring_Without_Repeating_Characters.java
@@ -5,9 +5,12 @@ import java.util.Set;
 
 class _03_Longest_Substring_Without_Repeating_Characters {
     public static void main(String[] argv) {
-        System.out.println(LengthOfLongestSubstring("ababcbb"));
-        System.out.println(LengthOfLongestSubstring("bbbb"));
-        System.out.println(LengthOfLongestSubstring("pwwkew"));
+        System.out.println("answer should be 3: " + LengthOfLongestSubstring("ababcbb"));
+        System.out.println("answer should be 1: " + LengthOfLongestSubstring("bbbb"));
+        System.out.println("answer should be 3: " + LengthOfLongestSubstring("pwwkew"));
+        System.out.println("answer should be 3: " + slidingWindow("ababcbb"));
+        System.out.println("answer should be 1: " + slidingWindow("bbbb"));
+        System.out.println("answer should be 3: " + slidingWindow("pwwkew"));
     }
 
     // brute force solution, Time O(n^3)
@@ -34,4 +37,33 @@ class _03_Longest_Substring_Without_Repeating_Characters {
         }
         return true;
     }
+
+    // alot of repetition on checking allunique
+    // if [0 .. 2] is allunique then no need to check [0..2] again in the next
+    // iteration [0..3]
+    // maybe update the left index once the duplicate char is hit
+    // a b c a c b b
+    // i j
+    // remove a from set and then advance i
+
+    static int slidingWindow(String s) {
+        int ans = 0, i = 0, j = 0, n = s.length();
+        Set<Character> set = new HashSet<>();
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j));
+                j++;
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+        return ans;
+    }
+    // still some repetition when detecting a duplicate where i only advance 1 step
+    // in "pwwkew", when 2nd w is detected, i need to 2 steps where it causes
+    // reapetitive duplicate checks
+    // maybe use hashmap to save index, so i can advance whatever steps needed
+
 }
