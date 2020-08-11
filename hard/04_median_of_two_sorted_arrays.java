ef5817dca04363b3cb53f7378598d01c731a5667
Alec Chen <goodneighboralec@gmail.com>
2020-08-11 07:15:44 -0700

    improved to Space O(1), but still need to improve Time comeplexity O(m+n) to O(log(m+n)

--- hard/04_median_of_two_sorted_arrays.java
+++ hard/04_median_of_two_sorted_arrays.java
@@ -33,6 +33,41 @@ class solution {
      * still fails the leetcode testcases.
      * 
      */
+
+    // find the median by iterating to the total_length/2, Time O(m+n), Space O(1)
+    static double findMedianSol2(int[] A, int[] B) {
+        int a_len = A.length, b_len = B.length, total_len = a_len + b_len, m = 0, n = 0, p = 0, q = 0;
+        if (a_len == 0)// return B's median
+        {
+            if ((b_len & 1) == 0) { // even number arr
+                return (B[b_len / 2 - 1] + B[b_len / 2]) / 2.0;
+            } else {
+                return (double) (B[b_len / 2]);
+            }
+        }
+        if (b_len == 0) {
+            if ((a_len & 1) == 0) {
+                return (A[b_len / 2 - 1] + A[b_len / 2]) / 2.0;
+            } else {
+                return (double) (A[b_len / 2]);
+            }
+        }
+        for (int i = 0; i < total_len / 2 + 1; i++) {
+            p = q;
+            if (m < a_len && (n >= b_len || A[m] < B[n])) {
+                q = A[m];
+                m++;
+            } else {
+                q = B[n];
+                n++;
+            }
+        }
+        if ((total_len & 1) == 0) {
+            return (p + q) / 2.0;
+        } else
+            return q;
+    }
+
     // brute force Time: O(m+n) Space: O(m+n)
     static double findMedianSol1(int[] A, int[] B) {
         int a_len = A.length, b_len = B.length;
@@ -80,8 +115,8 @@ class solution {
     }
 
     public static void main(String[] argv) {
-        System.out.println(findMedianSol1(new int[] { 1, 2 }, new int[] { 3, 4 }));
-        System.out.println(findMedianSol1(new int[] { 1, 2 }, new int[] { 3 }));
+        System.out.println(findMedianSol2(new int[] { 1, 2 }, new int[] { 3, 4 }));
+        System.out.println(findMedianSol2(new int[] { 1, 2 }, new int[] { 3 }));
 
     }
 }
