9cb8c9fe4bbc060f72086440749f8a4776578f32
Alec Chen <goodneighboralec@gmail.com>
2022-05-11 03:30:15 -0700

    revisit 371, adding thought process and recode the problem again

--- medium/371_getSum.java
+++ medium/371_getSum.java
@@ -9,32 +9,70 @@ class _371_getSum {
      * 
      * source: https://www.lintcode.com/problem/a-b-problem/description
      * 
-     * 
-     * 
-     * bitshifting (1. find carries, 2. do the addition using ^, 3. make b hold
-     * left_shifted carry) bit operator:
-     * 
-     * & -> 'AND' 1 1 => 1, 0 1 => 0, 0 0 => 0 to discover carry overs e.g 11 11 +
-     * ---- 11 ^ -> 'XOR' 1 1 => 0, 0 1 => 1, 1 0 => 1, 0 0 => 0 to simulate the
-     * addition
-     * 
-     * << -> left shift
-     * 
      */
     public static void main(String[] args) {
-        System.out.println(aplusb(13, 13));
+        int a = 9, b = 11;
+        System.out.println("expected: 20");
+        int ans = solution.getsum(a, b);
+        System.out.printf("output: %d\n", ans);
 
     }
 
-    public static int aplusb(int a, int b) {
-        return (b == 0) ? a : aplusb(a ^ b, (a & b) << 1);
-        // while (b != 0) {
-        // int carry = a & b; // find carries positions
-        // a ^= b; // actual addition without carries
-        // // make b = value where carry-over needed to be done in next iteration
-        // b = carry << 1;
-        // }
-        // return a;
+}
 
+class solution {
+
+    /*
+     * thoughts:
+     * this question is about bit manipulation and simulate a hardware adder, using
+     * half adder logic.
+     *
+     * AND & gate:
+     * x y x&y
+     * 0 0 0
+     * 0 1 0
+     * 1 0 0
+     * 1 1 1
+     * 
+     * XOR ^ gate:
+     * x y x^y
+     * 0 0 0
+     * 0 1 1
+     * 1 0 1
+     * 1 1 0
+     *
+     * 1. caculate the sum of x, y using xor ^
+     * 2. find if there is carry using and &, if yes, shift left by 1
+     * 3. xor ^ step 1 and 2
+     * 4. if there is carry, add shift left by 1 and xor ^ it back
+     * 4. repeat until there is no carry
+     * 
+     * stop condition: carry = 0
+     * 
+     * while loop version
+     * 
+     * int getsum(int x, int y){
+     * while (y != 0){
+     * int carry = x & y << 1;
+     * x = x & y;
+     * y = carry;
+     * }
+     * }
+     * 
+     * while loop not very intuitive to me, try recursion
+     * 
+     * int getsum(int x, int y)
+     * {
+     * if(y==0) return x;
+     * getsum(x^y, (x&y) <<1);
+     * }
+     * or 1 liner
+     * 
+     * y==0? x: getsum(x^y, (x&y) <<1) ;
+     * 
+     * 
+     */
+    public static int getsum(int a, int b) {
+        return b == 0 ? a : getsum(a ^ b, (a & b) << 1);
     }
 }
